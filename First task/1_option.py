1542e8b2ed645d33f5bb439eed9f15ab59c029e2
Vladislav <fedotov85050@gmail.com>
2021-10-29 01:24:40 +0300

    Right now ALL works.

--- First task/1_option.py
+++ First task/1_option.py
@@ -1,130 +1,180 @@
-import PyQt6.QtWidgets
 from text_to_num import text2num
 from spellchecker import SpellChecker
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(298, 286)
-        self.gridLayout = QtWidgets.QGridLayout(Dialog)
-        self.gridLayout.setContentsMargins(5, 0, 0, 0)
-        self.gridLayout.setSpacing(0)
-        self.gridLayout.setObjectName("gridLayout")
-        self.label_error = QtWidgets.QLabel(Dialog)
-        font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.label_error.setFont(font)
-        self.label_error.setObjectName("label_error")
-        self.gridLayout.addWidget(self.label_error, 0, 0, 1, 1)
-        self.label = QtWidgets.QLabel(Dialog)
-        font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.label.setFont(font)
-        self.label.setText("")
-        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
-        self.label_correction = QtWidgets.QLabel(Dialog)
-        font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.label_correction.setFont(font)
-        self.label_correction.setObjectName("label_correction")
-        self.gridLayout.addWidget(self.label_correction, 1, 0, 1, 1)
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.label_2.setFont(font)
-        self.label_2.setText("")
-        self.label_2.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 1, 1, 1, 1)
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Ошибка в слове"))
-        self.label_error.setText(_translate("Dialog", "Ошибка в слове:"))
-        self.label_correction.setText(_translate("Dialog", "Возможно вы\n"
-                                                           "имели ввиду:"))
-
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(382, 350)
-        MainWindow.setStyleSheet("")
+        MainWindow.resize(900, 500)
+        MainWindow.setMinimumSize(QtCore.QSize(900, 500))
+        MainWindow.setMaximumSize(QtCore.QSize(900, 500))
+        MainWindow.setStyleSheet("background-color: rgb(34, 34, 38);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName("gridLayout")
-        self.label_info = QtWidgets.QLabel(self.centralwidget)
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
+        self.frame.setObjectName("frame")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(270, -10, 354, 87))
         font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.label_info.setFont(font)
-        self.label_info.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.label_info.setObjectName("label_info")
-        self.gridLayout.addWidget(self.label_info, 0, 0, 1, 1)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label_number = QtWidgets.QLabel(self.centralwidget)
+        font.setPointSize(14)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label.setObjectName("label")
+        self.label_6 = QtWidgets.QLabel(self.frame)
+        self.label_6.setGeometry(QtCore.QRect(390, 200, 86, 29))
         font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.label_number.setFont(font)
-        self.label_number.setObjectName("label_number")
-        self.horizontalLayout.addWidget(self.label_number)
-        self.number_in_10cc = QtWidgets.QLabel(self.centralwidget)
+        font.setFamily("Literata 12pt")
+        font.setPointSize(14)
+        self.label_6.setFont(font)
+        self.label_6.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_6.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label_6.setObjectName("label_6")
+        self.layoutWidget = QtWidgets.QWidget(self.frame)
+        self.layoutWidget.setGeometry(QtCore.QRect(490, 250, 352, 171))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.gridLayout_9 = QtWidgets.QGridLayout(self.layoutWidget)
+        self.gridLayout_9.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_9.setObjectName("gridLayout_9")
+        self.labelTwoWordError = QtWidgets.QLabel(self.layoutWidget)
         font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.number_in_10cc.setFont(font)
-        self.number_in_10cc.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.number_in_10cc.setObjectName("number_in_10cc")
-        self.horizontalLayout.addWidget(self.number_in_10cc)
-        self.labe_rome = QtWidgets.QLabel(self.centralwidget)
+        font.setFamily("Literata 12pt")
+        font.setPointSize(14)
+        self.labelTwoWordError.setFont(font)
+        self.labelTwoWordError.setStyleSheet("color: rgb(255, 255, 255);")
+        self.labelTwoWordError.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.labelTwoWordError.setObjectName("labelTwoWordError")
+        self.gridLayout_9.addWidget(self.labelTwoWordError, 0, 0, 1, 1)
+        self.TwoWordError = QtWidgets.QLabel(self.layoutWidget)
+        self.TwoWordError.setMinimumSize(QtCore.QSize(350, 100))
+        self.TwoWordError.setMaximumSize(QtCore.QSize(301, 91))
         font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.labe_rome.setFont(font)
-        self.labe_rome.setObjectName("label_rome")
-        self.horizontalLayout.addWidget(self.labe_rome)
-        self.number_in_rome = QtWidgets.QLabel(self.centralwidget)
+        font.setPointSize(14)
+        self.TwoWordError.setFont(font)
+        self.TwoWordError.setStyleSheet("color: rgb(255, 255, 255);")
+        self.TwoWordError.setText("")
+        self.TwoWordError.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.TwoWordError.setObjectName("TwoWordError")
+        self.gridLayout_9.addWidget(self.TwoWordError, 1, 0, 1, 1)
+        self.layoutWidget1 = QtWidgets.QWidget(self.frame)
+        self.layoutWidget1.setGeometry(QtCore.QRect(10, 90, 251, 91))
+        self.layoutWidget1.setObjectName("layoutWidget1")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.layoutWidget1)
+        self.gridLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.lineEdit_enter = QtWidgets.QLineEdit(self.layoutWidget1)
         font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
         font.setPointSize(12)
-        self.number_in_rome.setFont(font)
-        self.number_in_rome.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.number_in_rome.setObjectName("number_in_rome")
-        self.horizontalLayout.addWidget(self.number_in_rome)
-        self.gridLayout.addLayout(self.horizontalLayout, 3, 0, 1, 1)
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        font.setUnderline(False)
+        font.setStrikeOut(False)
+        self.lineEdit_enter.setFont(font)
+        self.lineEdit_enter.setStyleSheet("border: 1px solid white;\n"
+                                          "text-decoration: none;\n"
+                                          "background-color: rgb(51, 51, 54);\n"
+                                          "color: rgb(255, 255, 255);")
+        self.lineEdit_enter.setObjectName("lineEdit_enter")
+        self.gridLayout_4.addWidget(self.lineEdit_enter, 0, 0, 1, 1)
+        self.pushButton_enter = QtWidgets.QPushButton(self.layoutWidget1)
         font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
-        self.lineEdit.setFont(font)
-        self.lineEdit.setObjectName("lineEdit")
-        self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 1)
-        self.pushButton_enter = QtWidgets.QPushButton(self.centralwidget)
-        font = QtGui.QFont()
-        font.setFamily("IBM Plex Sans")
-        font.setPointSize(12)
+        font.setPointSize(14)
         self.pushButton_enter.setFont(font)
+        self.pushButton_enter.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.pushButton_enter.setStyleSheet("QPushButton#pushButton_enter{\n"
+                                            "    background-color: rgb(51, 51, 54);\n"
+                                            "    border: 1px solid white;\n"
+                                            "    border-radius: 5px;\n"
+                                            "    color: rgb(255, 255, 255);\n"
+                                            "}\n"
+                                            "\n"
+                                            "QPushButton#pushButton_enter:pressed{\n"
+                                            "    background-color: rgb(40, 149, 0);\n"
+                                            "}\n"
+                                            "\n"
+                                            "\n"
+                                            "")
         self.pushButton_enter.setObjectName("pushButton_enter")
-        # ///
+        self.gridLayout_4.addWidget(self.pushButton_enter, 1, 0, 1, 1)
         self.pushButton_enter.clicked.connect(self.converter)
-        # ///
-        self.gridLayout.addWidget(self.pushButton_enter, 2, 0, 1, 1)
+        self.layoutWidget2 = QtWidgets.QWidget(self.frame)
+        self.layoutWidget2.setGeometry(QtCore.QRect(480, 80, 398, 97))
+        self.layoutWidget2.setObjectName("layoutWidget2")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.layoutWidget2)
+        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.gridLayout_2 = QtWidgets.QGridLayout()
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.label_4 = QtWidgets.QLabel(self.layoutWidget2)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_4.setFont(font)
+        self.label_4.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_4.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout_2.addWidget(self.label_4, 0, 2, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.layoutWidget2)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_3.setFont(font)
+        self.label_3.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_3.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_2.addWidget(self.label_3, 0, 0, 1, 1)
+        self.num_in_10cc = QtWidgets.QLabel(self.layoutWidget2)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.num_in_10cc.setFont(font)
+        self.num_in_10cc.setStyleSheet("color: rgb(255, 255, 255);")
+        self.num_in_10cc.setText("")
+        self.num_in_10cc.setObjectName("num_in_10cc")
+        self.gridLayout_2.addWidget(self.num_in_10cc, 0, 1, 1, 1)
+        self.num_in_rome = QtWidgets.QLabel(self.layoutWidget2)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.num_in_rome.setFont(font)
+        self.num_in_rome.setStyleSheet("color: rgb(255, 255, 255);")
+        self.num_in_rome.setText("")
+        self.num_in_rome.setObjectName("num_in_rome")
+        self.gridLayout_2.addWidget(self.num_in_rome, 0, 3, 1, 1)
+        self.gridLayout_3.addLayout(self.gridLayout_2, 1, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.layoutWidget2)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_2.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout_3.addWidget(self.label_2, 0, 0, 1, 1)
+        self.layoutWidget3 = QtWidgets.QWidget(self.frame)
+        self.layoutWidget3.setGeometry(QtCore.QRect(40, 250, 321, 171))
+        self.layoutWidget3.setObjectName("layoutWidget3")
+        self.gridLayout_5 = QtWidgets.QGridLayout(self.layoutWidget3)
+        self.gridLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_5.setObjectName("gridLayout_5")
+        self.labelErrorInWord = QtWidgets.QLabel(self.layoutWidget3)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.labelErrorInWord.setFont(font)
+        self.labelErrorInWord.setStyleSheet("color: rgb(255, 255, 255);")
+        self.labelErrorInWord.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.labelErrorInWord.setObjectName("labelErrorInWord")
+        self.gridLayout_5.addWidget(self.labelErrorInWord, 0, 0, 1, 1)
+        self.ErrorInWord = QtWidgets.QLabel(self.layoutWidget3)
+        self.ErrorInWord.setMinimumSize(QtCore.QSize(301, 100))
+        self.ErrorInWord.setMaximumSize(QtCore.QSize(301, 91))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.ErrorInWord.setFont(font)
+        self.ErrorInWord.setStyleSheet("color: rgb(255, 255, 255);")
+        self.ErrorInWord.setText("")
+        self.ErrorInWord.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.ErrorInWord.setObjectName("ErrorInWord")
+        self.gridLayout_5.addWidget(self.ErrorInWord, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -133,34 +183,47 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Перевод числа"))
-        self.label_info.setText(_translate("MainWindow", "Перевод числа из французского\n"
-                                                         "языка в 10СС и римские цифры."))
-        self.label_number.setText(_translate("MainWindow", "Число:"))
-        self.number_in_10cc.setText(_translate("MainWindow", ""))
-        self.labe_rome.setText(_translate("MainWindow", "Число в римском\n"
-                                                        "формате:"))
-        self.number_in_rome.setText(_translate("MainWindow", ""))
-        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Введите число на французском"))
-        self.pushButton_enter.setText(_translate("MainWindow", "Ввод"))
+        self.label.setText(_translate("MainWindow", "Перевод числа с французского языка\n"
+                                                    "в десятичное представление\n"
+                                                    "и римские цифры"))
+        self.label_6.setText(_translate("MainWindow", "Ошибки:"))
+        self.labelTwoWordError.setText(_translate("MainWindow", ""))
+        self.lineEdit_enter.setPlaceholderText(_translate("MainWindow", "Введите число на французском"))
+        self.pushButton_enter.setText(_translate("MainWindow", "Подтвердить"))
+        self.label_4.setText(_translate("MainWindow", "Римское\n"
+                                                      "число:"))
+        self.label_3.setText(_translate("MainWindow", "Число\n"
+                                                      " в 10СС:"))
+        self.label_2.setText(_translate("MainWindow", "Вывод:"))
+        self.labelErrorInWord.setText(_translate("MainWindow", ""))
 
     def converter(self):
-        self.word = self.lineEdit.text().lower()
+        self.word = self.lineEdit_enter.text().lower()
+
+        self.temp_word = self.word.split(' ')
+
+
+
         try:
             self.number = text2num(self.word, "fr")
         except ValueError:
             self.checker = SpellChecker(language='fr')
-            self.correct_word = self.checker.correction(self.word)
+            for idx, word in enumerate(self.temp_word):
+                self.correct_word = self.checker.correction(self.word)
+                if word != self.correct_word:
+                    self.labelErrorInWord.setText('Ошибка в слове')
+                    self.ErrorInWord.setText(word)
 
-            self.Dialog = QtWidgets.QDialog()
-            self.ui_dialog = Ui_Dialog()
-            self.ui_dialog.setupUi(self.Dialog)
-            self.Dialog.show()
+        for curr, next in zip(self.temp_word, self.temp_word[1:]):
+            if curr == next:
+                self.labelTwoWordError.setText('Одинаковые слова')
+                self.TwoWordError.setText(f'{curr}')
 
-            self.ui_dialog.label.setText(self.word)
-            self.ui_dialog.label_2.setText(self.correct_word)
+                self.labelErrorInWord.clear()
+                self.ErrorInWord.clear()
 
-        self.number_in_10cc.setText(str(self.number))
 
+        self.num_in_10cc.setText(str(self.number))
 
         self.val = [
             1000, 900, 500, 400,
@@ -182,8 +245,7 @@ class Ui_MainWindow(object):
                 self.roman_num += self.syb[self.i]
                 self.number -= self.val[self.i]
             self.i += 1
-        self.number_in_rome.setText(self.roman_num)
-
+        self.num_in_rome.setText(self.roman_num)
 
 
 if __name__ == "__main__":
